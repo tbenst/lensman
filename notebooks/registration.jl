7be22b30b9291c6d2d1801dc9624e2187f66ada2
tbenst <lensman@tylerbenster.com>
2020-10-06 01:18:43 -0700

    windows&unix

--- notebooks/registration.jl
+++ notebooks/registration.jl
@@ -9,14 +9,19 @@ using FileIO, RegisterQD, NRRD, ImageView, HDF5, MAT, Images,
 import Base.Threads.@threads
 using Unitful: μm, s
 
-# zbrain_dir = "O:\\users\\tyler\\zbrain\\"
-zbrain_dir = "/mnt/deissero/users/tyler/zbrain"
+zbrain_dir = ""
+tifdir = ""
+if Sys.iswindows()
+    tifdir = raw"E:\tyler\2020-10-05_elavl3-chrmine-kv2.1_h2b-6s\ZSeries-25X-default-exponential-zpower-64avg-034"
+    zbrain_dir = "O:\\users\\tyler\\zbrain\\"
+else
+    tifdir = "/mnt/deissero/users/tyler/b115/2020-09-29_elavl3-chrmine-kv2.1_h2-6s/Zseries_024034.17_-000"
+    zbrain_dir = "/mnt/deissero/users/tyler/zbrain"
+end
 zbrain_units = (0.798μm, 0.798μm, 2μm)
 
 # Read Zseries from file
 # tifdir = joinpath(pv.directory,"Zseries_024034.17_-000")
-# tifdir = raw"E:\tyler\2020-09-29_elavl3-chrmine-kv2.1_h2-6s\Zseries_024034.17_-000"
-tifdir = "/mnt/deissero/users/tyler/b115/2020-09-29_elavl3-chrmine-kv2.1_h2-6s/Zseries_024034.17_-000"
 tiff_files = joinpath.(tifdir, filter(x->(x[end-6:end]=="ome.tif") & occursin("Ch3", x),
     readdir(tifdir)))
 tif0 = ImageMagick.load(tiff_files[1])
@@ -32,6 +37,7 @@ size(zseries)
 #####
 # Read microns Per pixel (from file...)
 #####
+
 open(glob("*.xml",tifdir)[1], "r") do io
     zseries_xml = read(io, String)
     global zseries_xml = xp_parse(zseries_xml)
@@ -46,6 +52,10 @@ micronsPerPixel_xml = zseries_xml[xpath"""//PVStateValue[@key="micronsPerPixel"]
 lookup_μm = axis -> etree -> parse(Float64,
     micronsPerPixel_xml[xpath"""IndexedValue[@index="$axis"]"""][1].attr["value"])μm
 microscope_units = funprod(map(lookup_μm, ["YAxis", "XAxis", "ZAxis"])...)(micronsPerPixel_xml)
+
+@warn "temp hack for bad units"
+microscope_units = (0.5μm, 0.5μm, 1.5μm)
+
 # zseries = centered(zseries) # need centered for qd registration
 zseries = AxisArray(zseries, (:y, :x, :z), microscope_units)
 microscope_units
@@ -74,8 +84,9 @@ z_scaling = z_units/zbrain_z_units
 (zbrain_x_units, zbrain_y_units, zbrain_z_units), (z_units, y_units, x_units)
 
 init_y_translation = -(size(h2b_zbrain[Axis{:y}])[1] - size(zseries[Axis{:y}])[1]*y_scaling)/2
-init_x_translation = -50μm/x_units
-init_z_translation = -30
+# init_x_translation = -50μm/x_units
+init_x_translation = -70μm/x_units
+init_z_translation = -40
 
 # 3D preview pre-affine
 initial_tfm = Translation(init_y_translation, init_x_translation, init_z_translation)
@@ -86,11 +97,12 @@ replace_nan(v) = map(x -> isnan(x) ? zero(x) : x, v)
 zseries_registered = replace_nan.(warp(zseries, initial_affine,
     Base.axes(h2b_zbrain)));
 size(zseries_registered)
-##
+#
 im = RGB.(zeroToOne(h2b_zbrain))
 channelview(im)[2:3,:,:,:] .= 0
 channelview(im)[2,:,:,:] .= zseries_registered*300
 imshow(im)
+
 ##########
 ## ANTs
 ##########
