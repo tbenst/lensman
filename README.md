33b5740c3a407ca7833937811a3b22ffe1ab7b20
tbenst <lensman@tylerbenster.com>
2020-10-08 02:22:14 -0700

    ANTs now working

--- README.md
+++ README.md
@@ -27,3 +27,33 @@ downside: this won't work with remote-ssh :/
 
 ## papers:
 https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5597853/#!po=16.6667
+
+
+
+### ANTs debugging....
+#### example command
+/home/tyler/.julia/packages/ANTsRegistration/GZjzQ/deps/usr/bin/antsRegistration -d 3 --transform 'Affine[0.1]' --metric 'MI[/tmp/tyler/ANTs/3muoxnnd66.nrrd,/tmp/tyler/ANTs/dTmlyz29Lu.nrrd,1.0,32,Regular,0.25]' --convergence '[1000x500x250x0,1.0e-6,10]' --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox --output '[/tmp/tyler/ANTs/ztW88wKbBF_warp,/tmp/tyler/ANTs/ztW88wKbBF.nrrd]'
+
+LOOK for ztW88wKbBF.nii.gz. Then apply with `antsApplyTransforms`
+
+-j, --save-state saveSateAsTransform
+          A boolean specifying whether or not the composite transform resulted from the 
+          current state of the registration should be written to an hdf5 composite file. 
+          It is specially usefull if we want to save the current state of a SyN 
+          registration to the disk, so we can load and restore that later to continue the 
+          next registration directly started from the same state that it was stored. The 
+          output of this flag will be the same as the write-composite-transform, unless 
+          the last transform is a SyN transform. In that case, the inverse displacement 
+          field of the SyN transform is also added to the output composite transform. To 
+          load the written file this flag, we must use restore-state.
+
+
+## NixOS (IMPORTANT)
+
+Ants must be installed in develop mode, then after bulid, modify deps.jl (/home/tyler/.julia/dev/ANTsRegistration/deps/deps.jl) 
+```
+const ants_dir = read(`nix eval --raw "nixpkgs.ants"`, String)
+const ants = joinpath(ants_dir, "bin/ANTS")
+const antsRegistration = joinpath(ants_dir, "bin/antsRegistration")
+const antsMotionCorr = joinpath(ants_dir, "bin/antsMotionCorr")
+```
--- notebooks/registration.jl
+++ notebooks/registration.jl
@@ -1,13 +1,19 @@
 # file to load zseries from Oak, register to z-brain using ANTs
 # and finally segment cells using a watershed
-
+# how can we use PyCall but not call this binary...?
+# why aren't we in a conda-shell :/
+##
+true
+##
 ENV["DISPLAY"] = "localhost:11.0"
 using FileIO, RegisterQD, NRRD, ImageView, HDF5, MAT, Images,
     Unitful, AxisArrays, StaticArrays, CoordinateTransformations,
     ImageView, TestImages, NRRD, LinearAlgebra, ImageMagick,
-    LibExpat, Glob, ANTsRegistration, ImageSegmentation
+    LibExpat, Glob, ANTsRegistration, ImageSegmentation, PyCall,
+    NIfTI
 import Base.Threads.@threads
 using Unitful: μm, s
+sio = pyimport("scipy.io")
 
 zbrain_dir = ""
 tifdir = ""
@@ -15,7 +21,7 @@ if Sys.iswindows()
     tifdir = raw"E:\tyler\2020-10-05_elavl3-chrmine-kv2.1_h2b-6s\ZSeries-25X-default-exponential-zpower-64avg-034"
     zbrain_dir = "O:\\users\\tyler\\zbrain\\"
 else
-    tifdir = "/mnt/deissero/users/tyler/b115/2020-09-29_elavl3-chrmine-kv2.1_h2-6s/Zseries_024034.17_-000"
+    tifdir = "/mnt/deissero/users/tyler/b115/2020-10-05_elavl3-chrmine-kv2.1_h2b-6s/ZSeries-25X-default-exponential-zpower-64avg-034"
     zbrain_dir = "/mnt/deissero/users/tyler/zbrain"
 end
 zbrain_units = (0.798μm, 0.798μm, 2μm)
@@ -45,7 +51,6 @@ end;
 
 "apply each function to args"
 funprod(functions...) = (args...)->map(x->x(args...), functions)
-zeroToOne(x) = (x.-minimum(x))/(minimum(x) + maximum(x))
 
 micronsPerPixel_xml = zseries_xml[xpath"""//PVStateValue[@key="micronsPerPixel"]"""][1]
 # parse xml
@@ -98,25 +103,89 @@ zseries_registered = replace_nan.(warp(zseries, initial_affine,
     Base.axes(h2b_zbrain)));
 size(zseries_registered)
 #
-im = RGB.(zeroToOne(h2b_zbrain))
+im = RGB.(imadjustintensity(h2b_zbrain))
 channelview(im)[2:3,:,:,:] .= 0
 channelview(im)[2,:,:,:] .= zseries_registered*300
-imshow(im)
-
+# imshow(im)
+true
 ##########
 ## ANTs
-##########
+# parameters from https://academic.oup.com/gigascience/article/6/8/gix056/4085311#supplementary-data
+function marquart_register(fixed::AbstractArray, moving::AbstractArray)
+    fixedname = ANTsRegistration.write_nrrd(fixed)
+    movingname = ANTsRegistration.write_nrrd(moving)
+    outname = ANTsRegistration.randstring(10)
+    cmd = `antsRegistration -d 3 --float -o \[$outname\_, $outname\_Warped.nii.gz\] --interpolation WelchWindowedSinc --use-histogram-matching 0 -r \[$fixedname, $movingname,1\] -t rigid\[0.1\] -m MI\[$fixedname, $movingname,1,32, Regular,0.25\] -c \[200x200x200x0,1e-8,10\] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox -t Affine\[0.1\] -m MI\[$fixedname, $movingname,1,32, Regular,0.25\] -c \[200x200x200x0,1e-8,10\] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox -t SyN\[0.05,6,0.5\] -m CC\[$fixedname, $movingname,1,2\] -c \[200x200x200x200x10,1e-7,10\] --shrink-factors 12x8x4x2x1 --smoothing-sigmas 4x3x2x1x0vox`
+    # cmd = `antsRegistration -h`
+    read(cmd, String)
+end
+
+fixed = h2b_zbrain
+moving = zseries
+tmppath = ANTsRegistration.userpath()
+fixedname = joinpath(tmppath, ANTsRegistration.write_nrrd(fixed))
+movingname = joinpath(tmppath, ANTsRegistration.write_nrrd(moving))
+outprefix = ANTsRegistration.randstring(10)
+outname = joinpath(tmppath, outprefix)
+# no SyN so faster, but kinda meh registration...
+cmd = `antsRegistration -d 3 --float -o \[$outname\_, $outname\_Warped.nii.gz\] --interpolation WelchWindowedSinc --use-histogram-matching 0 -r \[$fixedname, $movingname,1\] -t rigid\[0.1\] -m MI\[$fixedname, $movingname,1,32, Regular,0.25\] -c \[200x200x200x0,1e-8,10\] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox -t Affine\[0.1\] -m MI\[$fixedname, $movingname,1,32, Regular,0.25\] -c \[200x200x200x0,1e-8,10\] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox`
+    # cmd = `antsRegistration -h`
+println(read(cmd, String))
+
+# `antsRegistration -d 3 –float 1 -o [fish1_, fish1_Warped.nii.gz] –interpolation WelchWindowedSinc –use-histogram-matching 0 -r [ref/vglut-ref.nii, fish1–01.nii.gz,1] -t rigid[0.1] -m MI[ref/vglut-ref.nii, fish1–01.nii.gz,1,32, Regular,0.25] -c [200 × 200 × 200 × 0,1e-8,10] –shrink-factors 12 × 8 × 4 × 2 –smoothing-sigmas 4 × 3 × 2 × 1vox -t Affine[0.1] -m MI[ref/vglut-ref.nii, fish1–01.nii.gz,1,32, Regular,0.25] -c [200 × 200 × 200 × 0,1e-8,10] –shrink-factors 12 × 8 × 4 × 2 –smoothing-sigmas 4 × 3 × 2 × 1vox -t SyN[0.05,6,0.5] -m CC[ref/vglut-ref.nii, fish1–01.nii.gz,1,2] -c [200 × 200 × 200 × 200 × 10,1e-7,10] –shrink-factors 12 × 8 × 4 × 2 × 1 –smoothing-sigmas 4 × 3 × 2 × 1 × 0vox `
+
+# ERROR:  The number of smoothing sigmas does not match the number of levels.
+# println(marquart_register(h2b_zbrain, zseries_registered))
+# println(fast_register(h2b_zbrain, zseries_registered))
+##
+affine_transform_path = glob(outprefix*"*GenericAffine.mat", tmppath)[1]
+affine_transform = sio.loadmat(affine_transform_path)
+# warpedname = joinpath(outname*".nrrd")
+warpedname = joinpath(outname*"_Warped.nii.gz")
+# TODO: make NIfTI use CodecZlib instead...
+registered = niread(warpedname)
+
+im = RGB.(imadjustintensity(h2b_zbrain))
+channelview(im)[2:3,:,:,:] .= 0
+channelview(im)[2,:,:,:] .= imadjustintensity(registered)
+imshow(im)
+
+##
+2*4
+##
+
+
+
 stageaff = Stage(h2b_zbrain, Global("Affine"))
 stagesyn = Stage(h2b_zbrain, SyN())
-imgw = register(h2b_zbrain, zseries_registered, [stageaff, stagesyn])
+up = ANTsRegistration.userpath()
+outname = ANTsRegistration.randstring(10)
+tfmname = joinpath(up, outname*"_warp")
+warpedname = joinpath(up, outname*".nrrd")
+output = (tfmname, warpedname)
+
+
+register(output, h2b_zbrain, zseries_registered,
+    [stageaff, stagesyn])
+    # [stageaff])
+##
+# TODO: is this v4 .mat file...?
+# affine_transform = matread(glob(outname*"*GenericAffine.mat", up)[1])
+affine_transform = sio.loadmat(glob(outname*"*GenericAffine.mat", up)[1])
+##
+run(`antsApplyTransforms -h`)
+# TODO: use antsApplyTransforms to tranfrom masks to our fish
+##
+imgw = load(warpedname)
+# TODO: warp 
 # crashes on reading in nrrd due to zlib.so not found :/ 
 # my LD_LIBRARY_PATH is effed..... due to conda shell
 # replicate crash
 # regular juila-env works!
-imshow(imgw)
+# imshow(imgw)
 ##
 # show registration onto our brain...
-im = RGB.(zeroToOne(h2b_zbrain))
+im = RGB.(imadjustintensity(h2b_zbrain))
 channelview(im)[2:3,:,:,:] .= 0
 channelview(im)[2,:,:,:] .= imgw*300
 imshow(im)
@@ -142,13 +211,13 @@ imshow(labels_map(seg))
 ##########
 
 ## AFFINE REGISTER
-zseries_affine = qd_affine(zeroToOne(h2b_zbrain),
-	zeroToOne(zseries), [100,100, 50];
+zseries_affine = qd_affine(imadjustintensity(h2b_zbrain),
+	imadjustintensity(zseries), [100,100, 50];
     initial_tfm=initial_affine)
 ##
 zseries_registered = warp(zseries, zseries_affine[1],
 	Base.axes(h2b_zbrain));
-im = RGB.(zeroToOne(h2b_zbrain))
+im = RGB.(imadjustintensity(h2b_zbrain))
 channelview(im)[2 .= 0
 channelview(im) .= zseries_registered*300
 im
@@ -161,7 +230,7 @@ imshow(zseries)
 ## BASELINE
 zseries_registered = warp(zseries[:,:,196], Translation((0.,0.)),
 	Base.axes(h2b_zbrain[:,:,100]));
-im = RGB.(zeroToOne(h2b_zbrain[:,:,100]))
+im = RGB.(imadjustintensity(h2b_zbrain[:,:,100]))
 channelview(im)[2:3,:,:] .= 0
 channelview(im)[2,:,:] .= zseries_registered*300
 im
