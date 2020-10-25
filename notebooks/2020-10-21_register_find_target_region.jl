b66a8f47372564d27d6a2d2145b172d75c45f90d
tbenst <lensman@tylerbenster.com>
2020-10-24 18:50:44 -0700

    update Date

--- notebooks/2020-10-21_register_find_target_region.jl
+++ notebooks/2020-10-21_register_find_target_region.jl
@@ -13,7 +13,7 @@ true
 import Pkg
 Pkg.activate(".")
 ##
-ENV["DISPLAY"] = "localhost:14"
+ENV["DISPLAY"] = "localhost:13"
 using FileIO, NRRD, ImageView, HDF5, MAT, Images,
     Unitful, AxisArrays, StaticArrays, CoordinateTransformations,
     ImageView, TestImages, NRRD, LinearAlgebra, ImageMagick,
@@ -33,7 +33,10 @@ else
     # tifdir = "/mnt/deissero/users/tyler/b115/2020-10-05_elavl3-chrmine-kv2.1_h2b-6s/ZSeries-25X-default-exponential-zpower-64avg-034/"
     # tifdir = "/mnt/deissero/users/tyler/b115/2020-10-20_elavl3-chrmine-kv2.1_h2b6s_6dpf/ZSeries-12112019-1026-002"
     # tifdir = "/mnt/deissero/users/tyler/b115/2020-10-21_elavl3-chrmine-kv2.1_h2b6s_dpf/ZSeries-12112019-1026-004"
-    tifdir = "/mnt/deissero/users/tyler/b115/2020-10-21_elavl3-chrmine-kv2.1_h2b6s_dpf/fish2/ZSeries-09172019-1403-036"
+    # tifdir = "/mnt/deissero/users/tyler/b115/2020-10-21_elavl3-chrmine-kv2.1_h2b6s_dpf/fish2/ZSeries-09172019-1403-036"
+    tifdir = "/mnt/deissero/users/tyler/b115/2020-10-22_elavl3-chrmine-Kv2.1_h2b6s_8dpf/fish1/ZSeries-09172019-1403-039"
+    tifdir = "/mnt/deissero/users/tyler/b115/2020-10-22_elavl3-chrmine-Kv2.1_h2b6s_8dpf/fish2/ZSeries-09172019-1403-041"
+
     zbrain_dir = "/mnt/deissero/users/tyler/zbrain"
 end
 zbrain_units = (0.798μm, 0.798μm, 2μm)
@@ -49,6 +52,7 @@ zseries = zeros(Normed{UInt16,16}, H, W, size(tiff_files, 1))
 @threads for z in 1:size(tiff_files,1)
     zseries[:,:,z] = ImageMagick.load(tiff_files[z])
 end
+# TODO: we should reverse atlas NOT our imaging to make creating .mat easier...
 zseries = reverse(zseries,dims=2); # face left
 zseries = reverse(zseries,dims=3); # bottom to top
 size(zseries)
@@ -72,12 +76,19 @@ lookup_μm = axis -> etree -> parse(Float64,
 microscope_units = funprod(map(lookup_μm, ["YAxis", "XAxis", "ZAxis"])...)(micronsPerPixel_xml)
 
 # convert to 25x units (16x is actually 14.4x, assumes Prairie View has 16x selected)
-@warn "assuming 16x units but using 25x objective"
-microscope_units = ((microscope_units[1:2] .* (14.4/25))..., microscope_units[3])
+# @warn "assuming 16x units but using 25x objective"
+# microscope_units = ((microscope_units[1:2] .* (14.4/25))..., microscope_units[3])
 # microscope_units = (0.5μm, 0.5μm, 1.5μm)
+
+# TODO: make Olympus 25x in bruker have proper units....
+@warn "hardcoded microscope units"
+microscope_units = (0.6299544139175637μm, 0.6299544139175637μm, 2.0μm)
 
 # zseries = centered(zseries) # need centered for qd registration
 zseries = AxisArray(zseries, (:y, :x, :z), microscope_units)
+adj_zseries = adjust_histogram(imadjustintensity(zseries), Equalization())
+adj_zseries = AxisArray(adj_zseries, (:y, :x, :z), microscope_units)
+@assert microscope_units[1] > 0.1μm
 microscope_units
 
 #####
@@ -86,46 +97,70 @@ microscope_units
 h2b_zbrain = AxisArray(permutedims(
         h5read("$zbrain_dir/AnatomyLabelDatabase.hdf5", "Elavl3-H2BRFP_6dpf_MeanImageOf10Fish"),
         (2,1,3)),
-    (:y, :x, :z), (0.798μm, 0.798μm, 2μm))
+    (:y, :x, :z), zbrain_units)
 ps = pixelspacing(h2b_zbrain); SD = ps./maximum(ps)
+adj_h2b_zbrain = adjust_histogram(imadjustintensity(h2b_zbrain), Equalization())
+adj_h2b_zbrain = AxisArray(adj_h2b_zbrain, (:y, :x, :z), zbrain_units)
 size(h2b_zbrain)
+# ## Examine our registration starting point...
+# im = RGB.(adjust_histogram(imadjustintensity(h2b_zbrain), Equalization()))
+# channelview(im)[2:3,:,:,:] .= 0
+# # channelview(im)[2,:,:,:] .= adj_zseries
+# imshow(im)
+# # warp(zseries[:,:,196], Translation((0.,0.))
+#
 
 
 ######
-# Register Zbrain to Zseries using ANTs
+## Register Zbrain to Zseries using ANTs
 ######
 
 # ONLY AFFINE for speed...
+# moving = h2b_zbrain
+# fixed = zseries
+# moving = adj_h2b_zbrain
 moving = h2b_zbrain
-fixed = zseries
+fixed = adj_zseries
 tmppath = ANTsRegistration.userpath()
 fixedname = joinpath(tmppath, ANTsRegistration.write_nrrd(fixed))
 movingname = joinpath(tmppath, ANTsRegistration.write_nrrd(moving))
 outprefix = ANTsRegistration.randstring(10)
 outname = joinpath(tmppath, outprefix)
+# outname = joinpath(tmppath, outprefix*"_SyN")
 println("creating file: $outname")
 # no SyN so faster, but kinda meh registration...
 maxiter = 100
 threshold = "1e-6"
-cmd = `antsRegistration -d 3 --float -o \[$outname\_, $outname\_Warped.nii.gz, $outname\_WarpedInv.nii.gz\] --interpolation WelchWindowedSinc --use-histogram-matching 0 -r \[$fixedname, $movingname,1\] -t rigid\[0.1\] -m MI\[$fixedname, $movingname,1,32, Regular,0.25\] -c \[$maxiter\x$maxiter\x$maxiter\x0,$threshold,10\] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox -t Affine\[0.1\] -m MI\[$fixedname, $movingname,1,32, Regular,0.25\] -c \[$maxiter\x$maxiter\x$maxiter\x0,$threshold,10\] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox`
+synThreshold = "1e-5"
+# originally was -r instead of -q
+# maybe try both?
+# -q is --initial-fixed-transform
+# -r is --initial-moving-transform
+# no SyN
+cmd = `antsRegistration -d 3 --float -o \[$outname\_, $outname\_Warped.nii.gz, $outname\_WarpedInv.nii.gz\] --interpolation WelchWindowedSinc --use-histogram-matching 0 -r \[$fixedname, $movingname,0\] -t rigid\[0.1\] -m MI\[$fixedname, $movingname,1,32, Regular,0.25\] -c \[$maxiter\x$maxiter\x$maxiter\x0,$threshold,10\] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox -t Affine\[0.1\] -m MI\[$fixedname, $movingname,1,32, Regular,0.25\] -c \[$maxiter\x$maxiter\x$maxiter\x0,$threshold,10\] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox`
+# SyN - takes ~20 minutes, TODO can this be sped up??
+# cmd = `antsRegistration -d 3 --float -o \[$outname\_, $outname\_Warped.nii.gz, $outname\_WarpedInv.nii.gz\] --interpolation WelchWindowedSinc --use-histogram-matching 0 -r \[$fixedname, $movingname,1\] -t rigid\[0.1\] -m MI\[$fixedname, $movingname,1,32, Regular,0.25\] -c \[$maxiter\x$maxiter\x$maxiter\x0,$threshold,10\] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox -t Affine\[0.1\] -m MI\[$fixedname, $movingname,1,32, Regular,0.25\] -c \[$maxiter\x$maxiter\x$maxiter\x0,$threshold,10\] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox -t SyN\[0.05,6,0.5\] -m CC\[$fixedname, $movingname,1,2\] -c \[$maxiter\x$maxiter\x$maxiter\x0,$synThreshold,10\] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox`
 println(read(cmd, String))
 
+# affine_transform_path = glob(outprefix*"_SyN"*"*GenericAffine.mat", tmppath)[1]
 affine_transform_path = glob(outprefix*"*GenericAffine.mat", tmppath)[1]
 affine_transform = sio.loadmat(affine_transform_path)
 # warpedname = joinpath(outname*".nrrd")
 warpedname = joinpath(outname*"_Warped.nii.gz")
+
+# if this fails, tmpfs may be out of space
+# try `rm -rf /run/user/1000/tyler/ANTs/`
 zbrain_registered = niread(warpedname);
 println("finished registering!")
-adj_zbrain_registered = adjust_histogram(imadjustintensity(zbrain_registered), Equalization())
-adj_zseries = adjust_histogram(imadjustintensity(zseries), Equalization())
+adj_zbrain_registered = adjust_histogram(imadjustintensity(zbrain_registered), Equalization());
 
-# visualize results as sanity check
+## visualize results as sanity check
 im = RGB.(adj_zbrain_registered)
 channelview(im)[2:3,:,:,:] .= 0
 channelview(im)[2,:,:,:] .= adj_zseries
 imshow(im)
 
-# Read mask & transform to Zseries
+## Read mask & transform to Zseries
 # TODO: define our own hemisphere masks (split zbrain down the middle..?)
 masks = matread("$zbrain_dir/MaskDatabase.mat")
 raphe_mask = getMaskNameIncludes(masks, "Raphe")
@@ -155,9 +190,13 @@ step_size = (planeHab - zero_plane) * zseries_step_size
 numZplanes = size(zseries,3)
 # note that our zseries is inverted from PV...
 # TODO: flip this so more intuitive...?
-println("Set Z 'zero' to be plane $(numZplanes-zero_plane) and step size $step_size μm to image 3 planes")
+# TODO: why are we flipped lr...?
+# println("Set Z 'zero' to be plane $(numZplanes-zero_plane) and step size $step_size μm to image 3 planes")
+# we move up one plane (2um) to account for spectral shift
+println("Set first plane to  $(numZplanes-planeHab-1) and step size $step_size μm to image 3 planes")
 # TODO: read state from xml file and communicate with PV so we get zseries microns correct...?
 
+println("showing prairie view plane numbers")
 @show numZplanes - zero_plane
 @show numZplanes - planeHab
 @show numZplanes - planeRaphe
@@ -175,25 +214,6 @@ local_maxi = findNeurons(zseries[:,:,planeHab])
 @show sum(local_maxi)
 maxi_mask = (dilate ∘ dilate ∘ dilate)(local_maxi) .> 0
 Gray.(maxi_mask)
-
-## hab stim
-rescaled_img = (adjust_histogram(imadjustintensity(zseries), Equalization()))
-stim_img = RGB.(rescaled_img[:,:,planeHab])
-si = channelview(stim_img)
-si[[1,3],:,:] .= 0
-si[1,:,:] .= (dilate ∘ dilate)(local_maxi .& hab_mask[:,:,planeHab])
-stim_img
-
-## raphe stim
-local_maxi = findNeurons(zseries[:,:,planeRaphe])
-maxi_mask = (dilate ∘ dilate ∘ dilate)(local_maxi) .> 0
-rescaled_img = (adjust_histogram(imadjustintensity(zseries[:,:,planeRaphe]), Equalization()))
-stim_img = RGB.(rescaled_img)
-si = channelview(stim_img)
-si[[1,3],:,:] .= 0
-si[1,:,:] .= (dilate ∘ dilate)(local_maxi .& raphe_mask[:,:,planeRaphe])
-stim_img
-
 
 ## MAKE PROTOCOL
 # we image 3 planes: best hab, best raphe, and best compromise
@@ -218,48 +238,65 @@ plane_neurons = findNeurons(zseries[:,:,planeRaphe])
 raphe_neurons = plane_neurons .& raphe_mask[:,:,planeRaphe]
 @show sum(raphe_neurons)
 
-# sample consistent number of neurons
-nNeuronsForStim = 50
-right_hab_neurons = sample(findall(right_hab_neurons), (nNeuronsForStim,),replace=false)
-left_hab_neurons = sample(findall(left_hab_neurons), (nNeuronsForStim,),replace=false)
-raphe_neurons = sample(findall(raphe_neurons), (nNeuronsForStim,),replace=false)
+## sample consistent number of neurons
+nNeuronsForStim = 33
+right_hab_neuron_locs = findall(right_hab_neurons)
+nRHabNeurons = minimum([nNeuronsForStim, size(right_hab_neuron_locs,1)])
+right_hab_neuron_locs = sample(right_hab_neuron_locs, nRHabNeurons, replace=false)
+left_hab_neuron_locs = findall(left_hab_neurons)
+nLHabNeurons = minimum([nNeuronsForStim, size(left_hab_neuron_locs,1)])
+left_hab_neuron_locs = sample(left_hab_neuron_locs, nLHabNeurons, replace=false)
+raphe_neuron_locs = findall(raphe_neurons)
+nRapheNeurons = minimum([nNeuronsForStim, size(raphe_neuron_locs,1)])
+raphe_neuron_locs = sample(raphe_neuron_locs, nRapheNeurons, replace=false)
 
 # TODO:
-# - how to convert right_hab_neurons to Array...?
+# - how to convert right_hab_neuron_locs to Array...?
 # - transform into targets array, and call create_slm_stim
 # - allow targets to pass 3D array with z-offset
 # - create the file that Sean's randomized trials button makes (this way we control)
-
 habZoffset = (zero_plane - planeHab) * zseries_step_size * 10^-6
-right_hab_targets = vcat(Float64.(hcat(collect.(Tuple.(right_hab_neurons))...)),
-    ones(Float64,1,nNeuronsForStim)*habZoffset)'
+right_hab_targets = vcat(Float64.(hcat(collect.(Tuple.(right_hab_neuron_locs))...)),
+ones(Float64,1,nRHabNeurons)*habZoffset)'
 # must be in 512x512 image space!! -> cropping makes life hard :/
 right_hab_targets[:,[1,2]] ./= 2
 right_hab_targets = right_hab_targets[:,[2,1,3]]
+@warn "hack solution for the flip problem..."
+right_hab_targets[:,1] .= 512 .- right_hab_targets[:,1]
+right_hab_targets[:,2] .= 512 .- right_hab_targets[:,2]
 
-left_hab_targets = vcat(Float64.(hcat(collect.(Tuple.(left_hab_neurons))...)),
-ones(Float64,1,nNeuronsForStim)*habZoffset)'
+left_hab_targets = vcat(Float64.(hcat(collect.(Tuple.(left_hab_neuron_locs))...)),
+ones(Float64,1,nLHabNeurons)*habZoffset)'
 left_hab_targets[:,[1,2]] ./= 2
 left_hab_targets = left_hab_targets[:,[2,1,3]]
+left_hab_targets[:,1] .= 512 .- left_hab_targets[:,1]
+left_hab_targets[:,2] .= 512 .- left_hab_targets[:,2]
 
 rapheZoffset = (zero_plane - planeRaphe) * 2. * 10^-6
-raphe_targets = vcat(Float64.(hcat(collect.(Tuple.(raphe_neurons))...)),
-ones(Float64,1,nNeuronsForStim)*rapheZoffset)'
+raphe_targets = vcat(Float64.(hcat(collect.(Tuple.(raphe_neuron_locs))...)),
+ones(Float64,1,nRapheNeurons)*rapheZoffset)'
 raphe_targets[:,[1,2]] ./= 2
 raphe_targets = raphe_targets[:,[2,1,3]]
+raphe_targets[:,1] .= 512 .- raphe_targets[:,1]
+raphe_targets[:,2] .= 512 .- raphe_targets[:,2]
 
 create_slm_stim([left_hab_targets, right_hab_targets, raphe_targets],
-    "lrhab_raphe")
+    # "diff_num_neurons")
+    # "fish2_with_resting_stim_lrhab_raphe")
+    "fish2_with_resting_stim_lrhab_raphe_no_adj_h2b")
+    # "fish2_with_resting_stim_lrhab_raphe_SyN")
 
 ## Visualize stim targets
 
 im = RGB.(adj_zseries)
 stim_points = zeros(Bool,size(adj_zseries))
-stim_points[right_hab_neurons,planeHab] .= true
-stim_points[left_hab_neurons,planeHab] .= true
-stim_points[raphe_neurons,planeRaphe] .= true
+stim_points[right_hab_neuron_locs,planeHab] .= true
+stim_points[left_hab_neuron_locs,planeHab] .= true
+stim_points[raphe_neuron_locs,planeRaphe] .= true
 stim_points = dilate(dilate(stim_points))
 channelview(im)[[1,3],:,:,:] .= 0
 channelview(im)[1,:,:,:] .= float(stim_points)
+channelview(im)[3,:,:,:] .= 0.5 * (hab_mask .| raphe_mask)
 imshow(im)
 @show planeHab, planeRaphe, zero_plane
+println("Set first plane to  $(numZplanes-planeHab-1) and step size $step_size μm to image 3 planes")
