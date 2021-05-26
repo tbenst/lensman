d9cb5311a029e983f427af8dd023e544e86a853d
Tyler Benster <github@tylerbenster.com>
2021-05-25 19:50:32 -0700

    adjustments for oak

--- notebooks/2021-05-18_standard_stim_characterization_shauld.jl
+++ notebooks/2021-05-18_standard_stim_characterization_shauld.jl
@@ -70,9 +70,11 @@ slmDir = "/oak/stanford/groups/deissero/users/tyler/b115/SLM_files"
 # tseriesDir = "$tseriesRootDir/2021-04-20_h33r-chrmine_6dpf_6f/fish2/TSeries-20cell-1concurrent-20trial-047"
 
 # tseriesDir = "$tseriesRootDir/2021-05-18_hsChRmine_h2b6s_6dpf/fish4/TSeries-lrhab-control-91trial-4Mhz-044"
-# tseriesDir = "$tseriesRootDir/2021-05-18_rsChRmine_h2b6s_6dpf/fish1/TSeries-lrhab-control-91trial-4Mhz-047"
+# tseriesDir = "$tseriesRootDir/2021-05-18_rsChRmine_h2b6s_6dpf/fish1/TSeries-lrhab-control-91trial-4Mhz-047" # missing tiffs...?
+tseriesDir = "$tseriesRootDir/2021-05-18_rsChRmine_h2b6s_6dpf/fish3/TSeries-lrhab-control-91trial-4Mhz-057" # ERROR on tiff header :(
 # tseriesDir = "$tseriesRootDir/2021-05-18_rsChRmine_h2b6s_6dpf/fish5/TSeries-lrhab-control-91trial-4Mhz-045"
-tseriesDir = "$tseriesRootDir/2021-05-18_rsChRmine_h2b6s_6dpf/fish5/TSeries-35cell-20rep-40s-dark-4Mhz-059"
+# tseriesDir = "$tseriesRootDir/2021-05-18_rsChRmine_h2b6s_6dpf/fish5/TSeries-35cell-20rep-40s-dark-4Mhz-059"
+# tseriesDir = "$tseriesRootDir/2021-05-18_rsChRmine_h2b6s_6dpf/fish5/TSeries-35cell-20rep-40s-dark-4Mhz-059"
 
 # debug by looking at 3region..?
 # tseriesDir = "$tseriesRootDir/2020-10-28_elavl3-chrmine-Kv2.1_h2b6s_8dpf/fish2/TSeries_lrhab_raphe_40trial-044/"
@@ -165,7 +167,13 @@ catch
 end
 
 ## read power
-slmTxtFile = regex_glob(r".*(?<!trialOrder)\.txt$", tylerSLMDir)
+try
+    global slmTxtFile
+    slmTxtFile = regex_glob(r".*(?<!trialOrder)\.txt$", tylerSLMDir)
+catch
+    global slmTxtFile
+    slmTxtFile = regex_glob(r".*(?<!trialOrder)\.txt$", fishDir)
+end
 @assert length(slmTxtFile) == 1 slmTxtFile # if not, need to be careful to choose
 slmTxtFile = slmTxtFile[1]
 
@@ -189,25 +197,18 @@ elseif slmNum == 2
 end
 
 if length(voltageFiles) == 1
-    stimStartIdx, stimEndIdx, frameStartIdx = getStimTimesFromVoltages(voltageFiles[1], Z);
+    stimStartIdx, stimEndIdx = getStimTimesFromVoltages(voltageFiles[1], Z)
 else
-    @warn "HARDCODED TperTrial"
-    TperTrial = Int(T / 20)
-    stimStartIdx, stimEndIdx, frameStartIdx = getStimTimesFromVoltages(voltageFiles, Z, TperTrial);
+    stimStartIdx, stimEndIdx = getStimTimesFromVoltages(voltageFiles, Z)
 end
-
-if length(voltageFiles) == 1
-    allWithin(diff(stimStartIdx),0.05)
-    # adjust for expected number of stimuli....
-    # @assert length(stimStartIdx) == 32
-    @show length(stimStartIdx)
-    allWithin(stimEndIdx .- stimStartIdx,0.05)
-    avgStimDuration = mean(stimEndIdx .- stimStartIdx)
-    # adjust for volume rate / stim duration...
-    @assert 30 > avgStimDuration > 3
-else
-    @warn "multiple voltage files so not checking asserts in case a trial structure..?"
-end
+allWithin(diff(stimStartIdx),0.05)
+# adjust for expected number of stimuli....
+# @assert length(stimStartIdx) == 32
+@show length(stimStartIdx)
+allWithin(stimEndIdx .- stimStartIdx,0.05)
+avgStimDuration = mean(stimEndIdx .- stimStartIdx)
+# adjust for volume rate / stim duration...
+@assert 30 > avgStimDuration > 3
 
 # Assumes no sequence of stim
 # for 5Hz clock
@@ -238,13 +239,10 @@ lateral_unit = microscope_lateral_unit(W)
 targetSizePx = spiral_size(expDate, lateral_unit)
 
 ## influence maps
-max_frames = stimStartIdx[1]-1 
-max_time = (max_frames-1)/volRate # otherwise bad index issues
 if volRate > 10
     nseconds = 3
 else
     nseconds = 5
-    nseconds = minimum([nseconds, max_time])
 end
 pre = Int(ceil(nseconds*volRate))+1
 post = Int(ceil(nseconds*volRate))+1
@@ -265,8 +263,8 @@ figB = 1.6
 # figW,figH = (figB*1.1, figB)
 figW,figH = (figB*Z, figB)
 
-window = minimum([Int(ceil(3*volRate)), max_frames])
-@assert (window <= post) & (window <= pre)
+window = Int(ceil(3*volRate))
+@assert (window < post) & (window < pre)
 # @assert Z == 1
 cmax = 2.5
 cmin = -0.5
@@ -321,24 +319,12 @@ for stimNum in 1:nStimuli
     cbar_ax = fig.add_axes([0.97, 0.15, 0.0075, 0.7])
     # cbar = fig.colorbar(cim, ticks=[0,1,2], cax=cbar_ax)
     cbar = fig.colorbar(cim, cax=cbar_ax)
-    path = joinpath(plotDir,"$(recording_folder)_$(fish_name)_$(expName)_denoised_stim$stimNum")
+    path = joinpath(plotDir,"$(recording_folder)_$(fish_name)_$(expName)_stim$stimNum")
     @show path*".svg"
     fig.savefig(path*".svg", dpi=600)
     fig.savefig(path*".png", dpi=600)
 end
 
-## kalman denoise
-@time denoised = imageJkalmanFilter(tseries)
-##
-avg_stim_h5_path = joinpath(fishDir,expName*"_denoised_avgStim.h5")
-have_avg_stim_h5 = isfile(avg_stim_h5_path)
-if have_avg_stim_h5
-    avgStim = h5read(avg_stim_h5_path, "/block1");
-else
-    avgStim = trialAverage(denoised, stimStartIdx, stimEndIdx, trialOrder;
-        pre=pre, post=post);
-    h5write(joinpath(fishDir,expName*"_denoised_avgStim.h5"), "/block1", avgStim)
-end;
 
 ##
 stimLocs = map(CartesianIndex ∘ Tuple, eachrow(cells[:,[2,1]]))
